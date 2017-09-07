ba2317400cf4a7676a36a73c8a3eed33d20819ca
Ben Lynn <benlynn@gmail.com>
2017-09-07 10:16:28 -0700

    Kautz network.
    Remove status reports.
    
    On my laptop, faster than libp2p for 20 nodes.

--- grapevine.hs
+++ grapevine.hs
@@ -10,14 +10,13 @@ import Control.Monad
 import qualified Crypto.Hash.SHA256 as SHA256
 import Data.Char
 import Data.IP
+import Data.List
 import Data.List.Split
 import qualified Data.Map.Strict as M
-import Data.Maybe
 import Network.Socket hiding (send, recv)
 import Network.Socket.ByteString
 import Safe
 import System.IO
-import Text.Read
 
 data Message = Hello String | Peerage (M.Map String String) | Link | Blob ByteString deriving (Show, Read)
 
@@ -47,7 +46,7 @@ newGrapevine royal name port = do
   bind inSock sockAddr
   listen inSock 128
   inPort <- socketPort inSock
-  bc <- newBoundedChan 20
+  bc <- newBoundedChan 128
   emptyPeerage <- newMVar M.empty
   emptyNeighbours <- newMVar []
   Grapevine royal name inPort inSock bc emptyPeerage emptyNeighbours <$> newMVar M.empty
@@ -68,13 +67,9 @@ grapevineNoble king name port = do
   outSock <- reuseMyPort gv
   connect outSock (addrAddress $ head addrInfo)
   void $ send outSock $ pack $ show $ Hello name
-  -- bs <- recv outSock 4096
-  -- when (bs /= "OK") $ ioError $ userError "The King is dead?"
   close outSock
   void $ forkIO $ nobleLoop gv
   pure gv
-
-data StatusCode = OK | EFull | EBadPeerage | EBadType | EUnknown deriving (Enum, Bounded, Show, Eq)
 
 kingLoop :: Grapevine -> IO ()
 kingLoop gv = do
@@ -82,12 +77,11 @@ kingLoop gv = do
   void $ forkIO $ do
     putStrLn $ show peer
     bs <- recv sock 4096
-    case readMaybe $ unpack bs of
+    case readMay $ unpack bs of
       Just (Hello s) -> do
         ps <- takeMVar $ peerage gv
         putMVar (peerage gv) $ M.insert s peer ps
-        -- void $ send sock "OK"
-      _ -> pure () -- void $ send sock "E_BADHELLO"
+      _ -> putStrLn "BAD HELLO"
     close sock
   kingLoop gv
 
@@ -97,16 +91,6 @@ readPeerage m = Just $ M.map f m where
     [ip4, port] = splitOn ":" s
     host = toHostAddress $ read ip4
 
-report :: Handle -> StatusCode -> IO ()
-report h s = do
-  void $ hPutChar h $ chr $ fromEnum s
-  hFlush h
-
-getStatus :: Handle -> IO StatusCode
-getStatus h = do
-  i <- ord <$> hGetChar h
-  pure $ fromMaybe EUnknown $ toEnumMay i
-
 wire :: Handle -> ByteString -> IO ()
 wire h s = do
   let n = B.length s
@@ -114,8 +98,7 @@ wire h s = do
   let ds = map (chr . (`mod` 256) . div n) $ (256^) <$> [3, 2, 1, 0 :: Int]
   forM_ ds $ hPutChar h
   B.hPut h s
-  status <- getStatus h
-  when (status /= OK) $ putStrLn $ "BUG: " ++ show status
+  hFlush h
 
 procure :: Handle -> IO ByteString
 procure h = do
@@ -127,26 +110,77 @@ procure h = do
 socialize :: Grapevine -> IO ()
 socialize gv = do
   ps <- readMVar $ peerage gv
-  hs <- forConcurrently (M.assocs ps) $ \(s, sock) -> if s == myNetName gv then pure [] else pure <$> do
-    tmp <- socket AF_INET Stream 0
-    connect tmp sock
-    h <- socketToHandle tmp ReadWriteMode
-    wire h $ pack $ show Link
-    pure h
-  void $ swapMVar (neighbours gv) $ concat hs
+  let n = M.size ps
+  if n < 10 then do
+    hs <- forConcurrently (M.assocs ps) $ \(s, sock) -> if s == myNetName gv then pure [] else pure <$> do
+      tmp <- socket AF_INET Stream 0
+      connect tmp sock
+      h <- socketToHandle tmp ReadWriteMode
+      wire h $ pack $ show Link
+      pure h
+    void $ swapMVar (neighbours gv) $ concat hs
+  else if n <= 20 then kautz gv ps 4 1
+  else if n <= 30 then kautz gv ps 5 1
+  else if n <= 42 then kautz gv ps 6 1
+  else if n <= 80 then kautz gv ps 4 2
+  else if n <= 108 then kautz gv ps 3 3
+  else if n <= 150 then kautz gv ps 5 2
+  else undefined
 
-process :: Grapevine -> Handle -> ByteString -> IO ()
-process gv han b = do
+kautz :: Grapevine -> M.Map String SockAddr -> Int -> Int -> IO ()
+kautz gv ps m n = let
+  sz = (m + 1)*m^n
+  lim = M.size ps
+  i = M.findIndex (myNetName gv) ps
+  os = filter (/= i) $ nub $ (`mod` lim) <$> (kautzOut m n i ++ if lim + i < sz then kautzOut m n (lim + i) else [])
+  in do
+    when (sz > lim * 2) $ ioError $ userError "BUG! Kautz graph too big!"
+    hs <- forConcurrently ((`M.elemAt` ps) <$> os) $ \(_, sock) -> do
+      tmp <- socket AF_INET Stream 0
+      connect tmp sock
+      h <- socketToHandle tmp ReadWriteMode
+      wire h $ pack $ show Link
+      pure h
+    void $ swapMVar (neighbours gv) hs
+
+
+toKautz :: Int -> Int -> Int -> [Int]
+toKautz m n i = f [] n i where
+  f [] r k = f [k `mod` (m + 1)] (r - 1) (k `div` (m + 1))
+  f acc@(h:_) 0 k = (if k >= h then k + 1 else k):acc
+  f acc@(h:_) r k = f (x:acc) (r - 1) (k `div` m) where
+    y = k `mod` m
+    x = if y >= h then y + 1 else y
+
+fromKautz :: Int -> [Int] -> Int
+fromKautz m as = f 0 as where
+  f i (x:y:rest) = f (i * m + (if x > y then x - 1 else x)) (y:rest)
+  f i [y] = i * (m + 1) + y
+  f _ _   = undefined
+
+{-
+prop_kautz :: Int -> Int -> Bool
+prop_kautz m n = s == (fromKautz m . toKautz m n <$> s) where s = [0..(m+1)*m^n]
+-}
+
+kautzOut :: Int -> Int -> Int -> [Int]
+kautzOut m n i = fromKautz m . (t ++) . pure <$> filter (/= last t) [0..m]
+  where t = tail $ toKautz m n i
+
+kautzIn :: Int -> Int -> Int -> [Int]
+kautzIn m n i = fromKautz m . (:s) <$> filter (/= head s) [0..m]
+  where s = init $ toKautz m n i
+
+process :: Grapevine -> ByteString -> IO ()
+process gv b = do
   seen <- takeMVar $ seenTable gv
   let h = SHA256.hash b
   case M.lookup h seen of
     Nothing -> do
       putMVar (seenTable gv) $ M.insert h [] seen
       status <- tryWriteChan (blobChan gv) b
-      report han $ if status then OK else EFull
-    Just _ -> do
-      putMVar (seenTable gv) seen
-      report han OK
+      when (not status) $ putStrLn "FULL BUFFER"
+    Just _ -> putMVar (seenTable gv) seen
 
 nobleLoop :: Grapevine -> IO ()
 nobleLoop gv = do
@@ -154,21 +188,19 @@ nobleLoop gv = do
   void $ forkIO $ do
     h <- socketToHandle sock ReadWriteMode
     bs <- procure h
-    case readMaybe $ unpack bs of
-      Just Link -> do
-        report h OK
-        forever $ process gv h =<< procure h
+    case readMay $ unpack bs of
+      Just Link -> do  -- TODO: Check sender `elem` kautzIn.
+        forever $ process gv =<< procure h
       Just (Peerage m) -> do
         case readPeerage m of
-          Nothing -> report h EBadPeerage
+          Nothing -> putStrLn "BAD PEERAGE"
           Just ps -> do
             void $ swapMVar (peerage gv) ps
             print =<< readMVar (peerage gv)
             socialize gv
-            report h OK
       Just (Blob b) -> do
-        process gv h b
-      _ -> report h EBadType
+        process gv b
+      _ -> putStrLn "BAD MESSAGE"
     hClose h
   nobleLoop gv
 
